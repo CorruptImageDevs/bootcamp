0623d219154664a2d88a18fd62d085024f091826
Irvin Buendia <irvin.buendia1@gmail.com>
2021-10-22 13:48:28 -0500

    callback after refactor

--- timeout/app.js
+++ timeout/app.js
@@ -1,23 +1,51 @@
 
-setTimeout(() => {
-    document.body.style.backgroundColor = 'red';
-    setTimeout(() => {
-        document.body.style.backgroundColor = "orange";
-        setTimeout(() => {
-            document.body.style.backgroundColor = "yellow";
-            setTimeout(() => {
-                document.body.style.backgroundColor = "green";
-                setTimeout(() => {
-                    document.body.style.backgroundColor = "blue";
-                    setTimeout(() => {
-                        document.body.style.backgroundColor = "indigo";
-                        setTimeout(() => {
-                            document.body.style.backgroundColor = "violet";
-                        },1000)
-                    },1000) 
-                },1000) 
-            },1000)            
-        },1000)
-    }, 1000)
-}, 1000)
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'red';
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = "orange";
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = "yellow";
+//             setTimeout(() => {
+//                 document.body.style.backgroundColor = "green";
+//                 setTimeout(() => {
+//                     document.body.style.backgroundColor = "blue";
+//                     setTimeout(() => {
+//                         document.body.style.backgroundColor = "indigo";
+//                         setTimeout(() => {
+//                             document.body.style.backgroundColor = "violet";
+//                         },1000)
+//                     },1000) 
+//                 },1000) 
+//             },1000)            
+//         },1000)
+//     }, 1000)
+// }, 1000)
 
+const delayedColorChange = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext && doNext();
+    }, delay)
+}
+
+delayedColorChange('pink', 1000, () =>
+    delayedColorChange('orange', 1000, () => {
+        delayedColorChange('green', 1000, () =>{
+            delayedColorChange("indigo", 1000, () => {
+                console.log("done")
+            })
+        })
+    })
+);
+
+// common scenarios
+
+searchMoviesAPI('Harry Potter', () => {
+    saveToMyDB(movies, () => {
+            //if it works, run this:
+    }, () => {
+    //if it doesnt work, run this:
+    })
+ }, () => {
+        //if API is down, or request failed, run this
+    })
