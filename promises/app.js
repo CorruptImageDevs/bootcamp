aa1bc06cfa8cd20008a4eceb2a0f62bf239fd46f
Irvin Buendia <irvin.buendia1@gmail.com>
2021-10-22 14:24:31 -0500

    promises fixed

--- promises/app.js
+++ promises/app.js
@@ -1,13 +1,13 @@
-const fakeRequestCallback = (url, success, failures) => {
-    const delay = Math.floor(Math.random() * 4500) + 500;
-    setTimeout(() => {
-        if (delay > 4000) {
-            failure('Connection Timeout ;(')
-        } else {
-            success(`Heres is your fake data from ${url}`)
-        }
-    }, delay)
-}
+// const fakeRequestCallback = (url, success, failures) => {
+//     const delay = Math.floor(Math.random() * 4500) + 500;
+//     setTimeout(() => {
+//         if (delay > 4000) {
+//             failure('Connection Timeout ;(')
+//         } else {
+//             success(`Heres is your fake data from ${url}`)
+//         }
+//     }, delay)
+// }
 
 const fakeRequestPromise = (url) => {
     return new Promise((resolve, reject) => {
@@ -23,8 +23,30 @@ const fakeRequestPromise = (url) => {
 }
 
 
-fakeRequestCallback('books.com', function (){
-    console.log("it worked!")
-}, function () {
-    console.log("ERROR")
-})
+// fakeRequestCallback('books.com', 
+// function (response){
+//     console.log('it worked')
+//     console.log(response)
+//     fakeRequestCallback
+// }, function (err) {
+//     console.log("ERROR", err)
+// })
+
+const request = fakeRequestPromise('yelp.com/api/coffee');
+
+ request
+    .then(() => {
+        console.log("it worked!");
+        fakeRequestPromise('yelp.com/api/coffee/page2')
+        .then(() => {
+            console.log("promise 2 resolved")
+            console.log("it worked again")
+        })
+        .catch(() => {
+            console.log("OH NO, ERROR")
+            console.log("promise 2 not resolved")
+       })
+    })
+    .catch(() => {
+         console.log("OH NO, ERROR")
+    })
