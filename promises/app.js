99c68eeabe571e92b53d6b900fa9ca3f3d723a5d
Irvin Buendia <irvin.buendia1@gmail.com>
2021-10-25 11:23:41 -0500

    fixed delayed with promise

--- promises/app.js
+++ promises/app.js
@@ -9,18 +9,18 @@
 //     }, delay)
 // }
 
-const fakeRequestPromise = (url) => {
-    return new Promise((resolve, reject) => {
-        const delay = Math.floor(Math.random() * (4500)) + 500;
-        setTimeout(() => {
-            if (delay > 4000) {
-                reject('Connection Timeout :(')
-            } else {
-                resolve(`Here is your fake data from ${url}`)
-            }
-        }, delay)
-    })
-}
+// const fakeRequestPromise = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const delay = Math.floor(Math.random() * (4500)) + 500;
+//         setTimeout(() => {
+//             if (delay > 4000) {
+//                 reject('Connection Timeout :(')
+//             } else {
+//                 resolve(`Here is your fake data from ${url}`)
+//             }
+//         }, delay)
+//     })
+// }
 
 
 // fakeRequestCallback('books.com', 
@@ -51,21 +51,50 @@ const fakeRequestPromise = (url) => {
 //          console.log("OH NO, ERROR")
 //     })
 
-fakeRequestPromise('yelp.com/api/coffee/page1')
-.then((data) => {
-    console.log("IT WORKED!!! (PAGE1)")
-    console.log(data)
-    return fakeRequestPromise('yelp.com/api/coffee/page2')
-})
-.then((data) => {
-    console.log("IT WORKED!!! (PAGE2)")
-    console.log(data)
-    return fakeRequestPromise('yelp.com/api/coffee/page3')
-})
-.then ((data) => {
-    console.log("IT WORKED!!! (PAGE3)")
-    console.log(data)
-})
-.catch(() => {
-    console.log("OH NO IT FAILED!!!")
-})
+
+// promise chain
+// fakeRequestPromise('yelp.com/api/coffee/page1')
+// .then((data) => {
+//     console.log("IT WORKED!!! (PAGE1)")
+//     console.log(data)
+//     return fakeRequestPromise('yelp.com/api/coffee/page2')
+// })
+// .then((data) => {
+//     console.log("IT WORKED!!! (PAGE2)")
+//     console.log(data)
+//     return fakeRequestPromise('yelp.com/api/coffee/page3')
+// })
+// .then ((data) => {
+//     console.log("IT WORKED!!! (PAGE3)")
+//     console.log(data)
+// })
+// .catch(() => {
+//     console.log("OH NO IT FAILED!!!")
+// })
+
+
+//creating promises
+// new Promise((resolve, reject) => {
+//     resolve();
+// })
+
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const rand = Math.random();
+        setTimeout(() => {
+            if(rand < 0.7) {
+            resolve("YOUR FAKE DATA HERE");
+            }
+            reject('REQUEST ERROR')
+        }, 1000)
+    })
+}
+
+fakeRequest('/dogs/1')
+    .then((data) => {
+        console.log("DONE WITH REQUEST!")
+        console.log('data is', data)
+    })
+    .catch((err) => {
+        console.log("OH NO!" , err)
+    })
