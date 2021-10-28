b1f42ef94af447adc2d5abc8088b1ddebe4e8169
Irvin Buendia <irvin.buendia1@gmail.com>
2021-10-27 23:14:40 -0500

    async promise username and password explained

--- AsyncFunctions/app.js
+++ AsyncFunctions/app.js
@@ -1,17 +1,33 @@
 // async function hello(){
 // }
 
-const sing = async() => {
-    throw "OH NO, PROBLEM"
-    return 'LA LA LA LA';
+// const sing = async() => {
+//     throw "OH NO, PROBLEM"
+//     return 'LA LA LA LA';
+// }
+
+// sing()
+// .then((data) => {
+//     console.log("PROMSISE RESOLVED WITH", data)
+// })
+// .catch(err => {
+//     console.log("OH NO, PROMISE REJECTED!")
+//     console.log(err)
+// })
+
+const login = async (username, password) => {
+    if(!username || !password) throw "PASSWORD OR USERNAME IS WRONG"
+    if(password === 'password') return 'WELCOME'
+    throw 'Invalid Password'
 }
 
-sing()
-.then((data) => {
-    console.log("PROMSISE RESOLVED WITH", data)
+login('adony', 'password')
+.then(msg => {
+    console.log("LOGGED IN ")
+    console.log(msg)
 })
 .catch(err => {
-    console.log("OH NO, PROMISE REJECTED!")
+    console.log("ERROR")
     console.log(err)
 })
 
