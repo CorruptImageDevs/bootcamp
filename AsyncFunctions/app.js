786b892bb48af059a352e360ae7edecbe427c6bb
Irvin Buendia <irvin.buendia1@gmail.com>
2021-10-27 23:24:48 -0500

    color timeout with async

--- AsyncFunctions/app.js
+++ AsyncFunctions/app.js
@@ -15,19 +15,59 @@
 //     console.log(err)
 // })
 
-const login = async (username, password) => {
-    if(!username || !password) throw "PASSWORD OR USERNAME IS WRONG"
-    if(password === 'password') return 'WELCOME'
-    throw 'Invalid Password'
+// const login = async (username, password) => {
+//     if(!username || !password) throw "PASSWORD OR USERNAME IS WRONG"
+//     if(password === 'password') return 'WELCOME'
+//     throw 'Invalid Password'
+// }
+
+// login('adony', 'password')
+// .then(msg => {
+//     console.log("LOGGED IN ")
+//     console.log(msg)
+// })
+// .catch(err => {
+//     console.log("ERROR")
+//     console.log(err)
+// })
+
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
 }
 
-login('adony', 'password')
-.then(msg => {
-    console.log("LOGGED IN ")
-    console.log(msg)
-})
-.catch(err => {
-    console.log("ERROR")
-    console.log(err)
-})
+// delayedColorChange('red', 1000)
+//     .then(() => delayedColorChange('orange', 1000))
+//     .then(() =>delayedColorChange('orange', 1000))
+//     .then(() =>delayedColorChange('yellow', 1000))
+//     .then(() =>delayedColorChange('green', 1000))
+//     .then(() =>delayedColorChange('blue', 1000))
+//     .then(() =>delayedColorChange('indigo', 1000))
+//     .then(() =>delayedColorChange('violet', 1000))
+//     .then(() =>delayedColorChange('hotpink', 1000))
 
+
+    async function rainbow(){
+        await delayedColorChange('red', 1000)
+        await delayedColorChange('orange', 1000)
+        await delayedColorChange('yellow', 1000)
+        await delayedColorChange('Green', 1000)
+        await delayedColorChange('blue', 1000)
+        await delayedColorChange('indigo', 1000)
+        await delayedColorChange('violet', 1000)
+        return "ALL DONE"
+    }
+
+    // rainbow().then(() => console.log("END OF RAINBOW"))
+
+    async function printRainbow() {
+        await rainbow();
+        console.log("END OF RAINBOW")
+    }
+
+    printRainbow();
